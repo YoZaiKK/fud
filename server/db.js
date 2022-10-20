c7f9d104728287a13544bf6136789bd5fc47a3bc
Guillermo <memocargo@hotmail.com>
2022-10-19 21:10:16 -0500

    setting up

---
+++ server/db.js
@@ -0,0 +1,12 @@
+import { createPool } from 'mysql2/promise';
+
+// con this vamos a poder hacer consultas con un metodo llamado query
+export const pool = createPool({
+  
+})
+
+// pool.query(`CREATE TABLE IF NOT EXISTS users`)
+
+
+
+
