6efc137183220e262abbd76a2169ee708676c783
Guillermo <memocargo@hotmail.com>
2022-10-31 19:47:30 -0600

    bd creada y enlazada y rutas inicializadas

---
+++ server/controllers/taks.controllers.js
@@ -0,0 +1,15 @@
+export const getTasks = (req, res) => {
+	res.send("obteniendo tareas");
+};
+export const getTask = (req, res) => {
+	res.send("obteniendo una tarea");
+};
+export const createTasks = (req, res) => {
+	res.send("creando tareas");
+};
+export const updateTasks = (req, res) => {
+	res.send("actualizando tarea");
+};
+export const deleteTasks = (req, res) => {
+	res.send("eliminando tarea");
+};
--- server/db.js
+++ server/db.js
@@ -2,7 +2,11 @@ import { createPool } from 'mysql2/promise';
 
 // con this vamos a poder hacer consultas con un metodo llamado query
 export const pool = createPool({
-  
+  host: 'localhost',
+  port: 3306,
+  user: 'root',
+  password: 'memopassword',
+  database: 'taskdb'
 })
 
 // pool.query(`CREATE TABLE IF NOT EXISTS users`)
