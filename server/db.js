bb8640206e7a777553c3dff0533e72efd8a63965
YoZaiKK <memocargo@hotmail.com>
2022-11-23 11:30:41 -0600

    comentario agregado, upload para evitar merge

--- server/db.js
+++ server/db.js
@@ -1,7 +1,8 @@
 import { createPool } from 'mysql2/promise';
 
-// con this vamos a poder hacer consultas con un metodo llamado query
+// con this vamos a poder hacer consultas con un metodo query
 export const pool = createPool({
+  // sustituir pool con los datos de la base usada
   host: 'localhost',
   port: 3306,
   user: 'root',
@@ -9,8 +10,5 @@ export const pool = createPool({
   database: 'taskdb'
 })
 
-// pool.query(`CREATE TABLE IF NOT EXISTS users`)
-
-
-
+// pool.query(`CREATE TABLE IF NOT EXISTS users`) 
 
