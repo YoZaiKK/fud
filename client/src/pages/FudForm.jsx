fbdd5b1fbaa3fd07f810362f0c184c892d3637fa
YoZaiKK <memocargo@hotmail.com>
2022-12-13 13:27:51 -0600

    curp validado-

--- client/src/pages/FudForm.jsx
+++ client/src/pages/FudForm.jsx
@@ -8,182 +8,267 @@ import Container from "react-bootstrap/Container";
 import Accordion from "react-bootstrap/Accordion";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import Alert from "react-bootstrap/Alert"; 
+import Alert from "react-bootstrap/Alert";
 
 export const FudForm = () => {
-	const { createFud, getFud, updateFud } = useFuds();
-	const [fuds, setFuds] = useState({
-		title: "",
-		description: "",
-	});
-	const [contador, setContador] = useState(0);
-	// const navigate = useNavigate();
-	const params = useParams();
+  const { createFud, getFud, updateFud } = useFuds();
+  const [fuds, setFuds] = useState({
+    title: "",
+    description: "",
+  });  
+  let curpValido = false;
+  // const navigate = useNavigate();
+  const params = useParams();
 
-	useEffect(() => {
-		const loadFud = async () => {
-			if (params.id) {
-				const values = await getFud(params.id);
-				console.log(values);
-				setFuds({
-					title: values.title,
-					description: values.description,
-				});
-			}
-		};
-		loadFud();
-	}, []);
+  useEffect(() => {
+    const loadFud = async () => {
+      if (params.id) {
+        const values = await getFud(params.id);
+        console.log(values);
+        setFuds({
+          title: values.title,
+          description: values.description,
+        });
+      }
+    };
+    loadFud();
+  }, []);
 
-	const someFunc = () => {
-		setContador(contador + 1);
-		console.log(contador + 1);
-	};
+ 
+  //Función para validar una CURP
+  function curpValida(curp) {
+    var re = /^([A-Z][AEIOUX][A-Z]{2}\d{2}(?:0[1-9]|1[0-2])(?:0[1-9]|[12]\d|3[01])[HM](?:AS|B[CS]|C[CLMSH]|D[FG]|G[TR]|HG|JC|M[CNS]|N[ETL]|OC|PL|Q[TR]|S[PLR]|T[CSL]|VZ|YN|ZS)[B-DF-HJ-NP-TV-Z]{3}[A-Z\d])(\d)$/,
+      validado = curp.match(re);
 
-	return (
-		<Container>
-			<h1>{params.id ? "Edit Fud" : "Create Fud"}</h1>
-			<Formik
-				initialValues={fuds}
-				enableReinitialize={true}
-				onSubmit={async (values, actions) => {
-					console.log(values);
-					// params.id
-					// 	? await updateFud(params.id, values)
-					// 	: await createFud(values);
-					console.log(fuds);
-					setFuds({ title: "", description: "" });
-					// navigate("/");
-				}}
-			>
-				{({ handleChange, handleSubmit, values, isSubmitting }) => (
-					<Form onSubmit={handleSubmit}>
-						{/* trabajando en esto */}
-						<Accordion defaultActiveKey="0">
-							<Accordion.Item eventKey="0">
-								<Accordion.Header>Datos del denunciante</Accordion.Header>
-								<Accordion.Body>
-									<Container>
-										<Row>
-											<Form.Label>Registrante</Form.Label>
-										</Row>
-										<Row>
-											<Col>
-												<Form.Label>
-													<Field type="radio" name="registrante" value="Fam" />
-													Familiar
-												</Form.Label>
-											</Col>
-											<Col xs={5}>
-												<Form.Label>
-													<Field
-														type="radio"
-														name="registrante"
-														value="ServPub"
-													/>
-													Servidor publico o autoridad
-												</Form.Label>
-											</Col>
-											<Col></Col>
-										</Row>
-									</Container>
-									<Form.Group>
-										<Form.Label>Nombre completo </Form.Label>
-										<Form.Control
-											type="text"
-											name="title"
-											placeholder="Nombre(s), Primer apellido, Segundo apellido"
-											onChange={handleChange}
-											value={values.title}
-										/>
-									</Form.Group>
-									{values.registrante == "Fam" ? (
-										<Alert>
-											<Container>
-												<Row>
-													<Form.Label>¿Es el tutor de la victima?</Form.Label>
-												</Row>
-												<Row>
-													<Col>
-														<Form.Label>
-															<Field type="radio" name="tutor" value="Si" />
-															Si
-														</Form.Label>
-													</Col>
-													<Col xs={5}>
-														<Form.Label>
-															<Field type="radio" name="tutor" value="No" />
-															No
-														</Form.Label>
-													</Col>
-													<Col></Col>
-												</Row>
-											</Container>
-										</Alert>
-									) : (
-										<Alert>
-											<label>Servidor publico o autoridad</label>
-											<Form.Group>
-												<Form.Label>Nombre del cargo</Form.Label>
-												<Form.Control
-													type="text"
-													name="cargoServPub"
-													placeholder="Jefe de..."
-													onChange={handleChange} 
-												/>
-											</Form.Group>
+    if (!validado)  //Coincide con el formato general?
+      return false;
 
-											<Form.Group>
-												<Form.Label>Departamento</Form.Label>
-												<Form.Control
-													type="text"
-													name="depaServPub"
-													placeholder="Departamento de..."
-													onChange={handleChange} 
-												/>
-											</Form.Group>
-											<br />
-										</Alert>
-									)}
-								</Accordion.Body>
-							</Accordion.Item>
+    //Validar que coincida el dígito verificador
+    function digitoVerificador(curp17) {
+      //Fuente https://consultas.curp.gob.mx/CurpSP/
+      var diccionario = "0123456789ABCDEFGHIJKLMNÑOPQRSTUVWXYZ",
+        lngSuma = 0.0,
+        lngDigito = 0.0;
+      for (var i = 0; i < 17; i++)
+        lngSuma = lngSuma + diccionario.indexOf(curp17.charAt(i)) * (18 - i);
+      lngDigito = 10 - lngSuma % 10;
+      if (lngDigito == 10) return 0;
+      return lngDigito;
+    }
 
-							<Accordion.Item eventKey="1">
-								<Accordion.Header>Datos de la Victima</Accordion.Header>
-								<Accordion.Body>A</Accordion.Body>
-							</Accordion.Item>
+    if (validado[2] != digitoVerificador(validado[1]))
+      return false;
 
-							<Accordion.Item eventKey="2">
-								<Accordion.Header>Relacion con la victima</Accordion.Header>
-								<Accordion.Body>A</Accordion.Body>
-							</Accordion.Item>
+    return true; //Validado
+  }
+  return (
+    <Container>
+      <h1>{params.id ? "Edit Fud" : "Create Fud"}</h1>
+      <Formik
+        initialValues={fuds}
+        enableReinitialize={true}
+        onSubmit={async (values, actions) => {
+          console.log(values);
+          // params.id
+          // 	? await updateFud(params.id, values)
+          // 	: await createFud(values);
+          console.log(fuds);
+          setFuds({ title: "", description: "" });
+          // navigate("/");
+        }}
+      >
+        {({ handleChange, handleSubmit, values, isSubmitting }) => (
+          <Form onSubmit={handleSubmit}>
+            {/* trabajando en esto */}
+            <Accordion defaultActiveKey="0">
+              <Accordion.Item eventKey="0">
+                <Accordion.Header>Datos del denunciante</Accordion.Header>
+                <Accordion.Body>
+                  <Container>
+                    <Row>
+                      <Form.Label>Registrante</Form.Label>
+                    </Row>
+                    <Row>
+                      <Col>
+                        <Form.Label>
+                          <Field type="radio" name="registrante" value="Fam" />
+                          Familiar
+                        </Form.Label>
+                      </Col>
+                      <Col xs={5}>
+                        <Form.Label>
+                          <Field
+                            type="radio"
+                            name="registrante"
+                            value="ServPub"
+                          />
+                          Servidor publico o autoridad
+                        </Form.Label>
+                      </Col>
+                    </Row>
+                  </Container>
+                  <Form.Group>
+                    <Form.Label>Nombre completo </Form.Label>
+                    <Form.Control
+                      type="text"
+                      name="nombreRegistrante"
+                      placeholder="Nombre(s), Primer apellido, Segundo apellido"
+                      onChange={handleChange}
+                    />
+                  </Form.Group>
+                  {values.registrante == "Fam" ? (
+                    <Alert>
+                      <Container>
+                        <Row>
+                          <Form.Label>¿Es el tutor de la victima?</Form.Label>
+                        </Row>
+                        <Row>
+                          <Col>
+                            <Form.Label>
+                              <Field type="radio" name="tutor" value="Si" />
+                              Si
+                            </Form.Label>
+                          </Col>
+                          <Col xs={5}>
+                            <Form.Label>
+                              <Field type="radio" name="tutor" value="No" />
+                              No
+                            </Form.Label>
+                          </Col>
+                          <Col></Col>
+                        </Row>
+                      </Container>
+                    </Alert>
+                  ) : (
+                    <Alert>
+                      <label>Servidor publico o autoridad</label>
+                      <Form.Group>
+                        <Form.Label>Nombre del cargo</Form.Label>
+                        <Form.Control
+                          type="text"
+                          name="cargoServPub"
+                          placeholder="Jefe de..."
+                          onChange={handleChange}
+                        />
+                      </Form.Group>
 
-							<Accordion.Item eventKey="3">
-								<Accordion.Header>Observaciones</Accordion.Header>
-								<Accordion.Body>A</Accordion.Body>
-							</Accordion.Item>
+                      <Form.Group>
+                        <Form.Label>Departamento</Form.Label>
+                        <Form.Control
+                          type="text"
+                          name="depaServPub"
+                          placeholder="Departamento de..."
+                          onChange={handleChange}
+                        />
+                      </Form.Group>
+                      <br />
+                    </Alert>
+                  )}
+                </Accordion.Body>
+              </Accordion.Item>
 
-							<Accordion.Item eventKey="4">
-								<Accordion.Header>Seguimiento de denuncia</Accordion.Header>
-								<Accordion.Body>A</Accordion.Body>
-							</Accordion.Item>
+              <Accordion.Item eventKey="1">
+                <Accordion.Header>Datos de la Victima</Accordion.Header>
+                <Accordion.Body>
+                  <Container>
+                    <Row>
+                      <Form.Label>Registrante</Form.Label>
+                    </Row>
+                    <Row>
+                      <Col>
+                        <Form.Label>
+                          <Field type="radio" name="tipoVictima" value="directa" />
+                          Directa
+                        </Form.Label>
+                      </Col>
+                      <Col >
+                        <Form.Label>
+                          <Field
+                            type="radio"
+                            name="tipoVictima"
+                            value="indirecta"
+                          />
+                          Indirecta
+                        </Form.Label>
+                      </Col>
+                      <Col>
+                        <Form.Label>
+                          <Field
+                            type="radio"
+                            name="tipoVictima"
+                            value="potencial"
+                          />
+                          Potencial
+                        </Form.Label>
+                      </Col>
+                    </Row>
+                  </Container>
+                  <Form.Group>
+                    <Form.Label>Nombre completo </Form.Label>
+                    <Form.Control
+                      type="text"
+                      name="nombreVictima"
+                      placeholder="Nombre(s), Primer apellido, Segundo apellido"
+                      onChange={handleChange}
+                    />
+                    <Form.Label>Fecha de nacimiento </Form.Label>
+                    <Form.Control
+                      type="date"
+                      name="fechaNacimiento"
+                      onChange={handleChange}
+                    />
 
-							<Accordion.Item eventKey="5">
-								<Accordion.Header>Informacion adicional</Accordion.Header>
-								<Accordion.Body>A</Accordion.Body>
-							</Accordion.Item>
-						</Accordion>
-						<br />
-						<Button type="submit" disabled={isSubmitting}>
-							{isSubmitting ? "Saving..." : "Acept"}
-						</Button>{" "}
-						<br /> <br />
-						<Button type="button" onClick={someFunc} disabled={isSubmitting}>
-							{isSubmitting ? "Saving..." : "Acept "}
-							{contador}
-						</Button>
-					</Form>
-				)}
-			</Formik>
-		</Container>
-	);
+                    <Form.Label>CURP </Form.Label>
+                    <Form.Control
+                      type="text"
+                      name="curp"
+                      placeholder="Nombre(s), Primer apellido, Segundo apellido"
+                      onChange={handleChange}
+                      onInput={(e) => {
+                        console.log(e.target.value)
+                        curpValido = (curpValida(e.target.value))
+                      }}
+                    />
+                    <br />
+                    {curpValido ?
+                      null :
+                      <Alert variant="danger">
+                        CURP invalido
+                      </Alert>
+                    }
+ 
+                  </Form.Group>
+                </Accordion.Body>
+              </Accordion.Item>
+
+              <Accordion.Item eventKey="2">
+                <Accordion.Header>Relacion con la victima</Accordion.Header>
+                <Accordion.Body>A</Accordion.Body>
+              </Accordion.Item>
+
+              <Accordion.Item eventKey="3">
+                <Accordion.Header>Observaciones</Accordion.Header>
+                <Accordion.Body>A</Accordion.Body>
+              </Accordion.Item>
+
+              <Accordion.Item eventKey="4">
+                <Accordion.Header>Seguimiento de denuncia</Accordion.Header>
+                <Accordion.Body>A</Accordion.Body>
+              </Accordion.Item>
+
+              <Accordion.Item eventKey="5">
+                <Accordion.Header>Informacion adicional</Accordion.Header>
+                <Accordion.Body>A</Accordion.Body>
+              </Accordion.Item>
+            </Accordion>
+            <br />
+            <Button type="submit" disabled={isSubmitting}>
+              {isSubmitting ? "Saving..." : "Acept"}
+            </Button>{" "}
+            <br /> <br />
+          </Form>
+        )}
+      </Formik>
+    </Container>
+  );
 };
